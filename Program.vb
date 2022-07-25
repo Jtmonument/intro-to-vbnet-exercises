ecb34762df8753886bd5ad21628dcd4beeffc062
Jonathan Taylor <jontaylor091@gmail.com>
2022-07-24 21:12:16 -0500

    Assignment 2.2.1

--- Program.vb
+++ Program.vb
@@ -2,15 +2,9 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Dim sum As Integer
-        Do
-            Console.Write("Enter a number: ")
-            Dim input As Integer = Console.ReadLine()
-            If input = -1 Then
-                Exit Do
-            End If
-            sum += input
-        Loop
-        Console.WriteLine(String.Format("{0:#,#.00}", sum))
+        Dim myself As bmi = New bmi
+        myself.Height = 1.88
+        myself.Weight = 63.5
+        Console.WriteLine(myself.BMI_Value)
     End Sub
 End Module
