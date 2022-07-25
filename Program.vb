dbe37149161b0f6d21a07777ba066e06a4b71936
Jonathan Taylor <jontaylor091@gmail.com>
2022-07-25 00:12:28 -0500

    Assignment 3.2.1

--- Program.vb
+++ Program.vb
@@ -3,26 +3,12 @@ Imports System.IO
 
 Module Program
     Sub Main(args As String())
-        Dim indata As FileStream = New FileStream("indata.txt", FileMode.Create, FileAccess.Write)
-        Dim number As Random = New Random
-        Dim writer As StreamWriter = New StreamWriter(indata)
-        For i = 0 To 10
-            If i = 10 Then
-                writer.Write(number.Next(50))
-            Else
-                writer.Write(number.Next(50) & ", ")
-            End If
-        Next
-        writer.Close()
-
-        indata = New FileStream("indata.txt", FileMode.Open, FileAccess.Read)
-
-        Dim sum As Integer
-        Dim reader As StreamReader = New StreamReader(indata)
-        For Each num As Integer In reader.ReadLine().Split(", ")
-            sum += num
-        Next
-        reader.Close()
-        Console.WriteLine(sum)
+        Dim myself As bmi = New bmi
+        myself.Height = 1
+        myself.Weight = 1
+        Dim you As bmi = New bmi
+        you.Height = 1
+        you.Weight = 1
+        Console.WriteLine(myself.Equals(you))
     End Sub
 End Module
