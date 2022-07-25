2e2ae8a8b12f0ab6441358f18ee4224db66b4309
Jonathan Taylor <jontaylor091@gmail.com>
2022-07-24 20:09:21 -0500

    Assignment 2.1.2

--- Program.vb
+++ Program.vb
@@ -2,7 +2,15 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Console.Write("Enter a number: ")
-        Console.WriteLine(String.Format("{0:D}", DateTime.Now.AddDays(CDbl(Console.ReadLine()))))
+        Dim sum As Integer
+        Do
+            Console.Write("Enter a number: ")
+            Dim input As Integer = Console.ReadLine()
+            If input = -1 Then
+                Exit Do
+            End If
+            sum += input
+        Loop
+        Console.WriteLine(String.Format("{0:#,#.00}", sum))
     End Sub
 End Module
